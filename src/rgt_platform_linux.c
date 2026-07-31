c49329de25b76ce588843ce6e08da4339aa197fc
Guiroshix <bomberj2me@gmail.com>
2026-07-31 02:41:59 -0300

    Update rgt_platform_linux.c

--- src/rgt_platform_linux.c
+++ src/rgt_platform_linux.c
@@ -1,3 +1,7 @@
+#include "rgt_platform.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 rgt_result rgt_copy_file(const char *src, const char *dst)
 {
     char command[512];
@@ -5,6 +9,19 @@ rgt_result rgt_copy_file(const char *src, const char *dst)
     snprintf(command, sizeof(command),
         "cp \"%s\" \"%s\"",
         src, dst);
+
+    system(command);
+
+    return RGT_SUCCESS;
+}
+
+rgt_result rgt_umd_replace(const char *iso, const char *file, const char *replacement)
+{
+    char command[1024];
+
+    snprintf(command, sizeof(command),
+        "UMDReplaceK-Linux.bin \"%s\" \"%s\" \"%s\"",
+        iso, file, replacement);
 
     system(command);
 
