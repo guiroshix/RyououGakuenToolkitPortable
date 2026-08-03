71a723f373a312ba5ebab0cfbb71da9689bfd241
Guiroshix <bomberj2me@gmail.com>
2026-08-03 17:26:34 -0300

    Fixing my dumbass

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ make clean
 
 make -B
 
-chmod umd-replace-linux-x86_64 2>/dev/null
-chmod umd-replace-linux-arm64 2>/dev/null
+chmod +x umd-replace-linux-x86_64 2>/dev/null
+chmod +x umd-replace-linux-arm64 2>/dev/null
