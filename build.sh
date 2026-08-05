e05708ad21dae2f240476fe93aaefe58a03692d7
Guiroshix <bomberj2me@gmail.com>
2026-08-05 19:50:45 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+echo "Cleaning..."
 make clean
 
+echo "Building..."
 make -B
 
-chmod +x umd-replace-linux-x86_64 2>/dev/null
-chmod +x umd-replace-linux-arm64 2>/dev/null
+echo "Setting executable permissions..."
+chmod +x umd-replace-linux-x86_64 2>/dev/null || true
+chmod +x umd-replace-linux-arm64 2>/dev/null || true
+
+echo "Build complete!"
